fb5e2bdcfbdcdfc467e371d7e780a34907dd3218
Kirill <prituzhalovkirill@gmail.com>
2023-11-16 17:59:27 +0600

    commit 2

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
 
 export const Login = () => {
   const [valueEmail, setValueEmail] = React.useState('');
@@ -15,23 +21,42 @@ export const Login = () => {
     setValuePassword(event.target.value);
   };
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    defaultValues: {},
+  });
+
+  const submit: SubmitHandler<LoginForm> = (data) => {
+    console.log(data);
+  };
+
+  const error: SubmitErrorHandler<LoginForm> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="container">
       <div className="login">
-        <form>
+        <form onSubmit={handleSubmit(submit, error)}>
           <h3>Авторизация</h3>
           <div className="login__item login__email">
             <label>Электронная почта</label>
             <input
+              {...register('email', { required: true })}
               value={valueEmail}
               onChange={onChangeInputEmail}
               type="text"
               placeholder="example@mail.ru"
+              aria-invalid={errors.email ? true : false}
             />
           </div>
           <div className="login__item login__password">
             <label>Пароль</label>
             <input
+              {...register('password')}
               value={valuePassword}
               onChange={onChangeInputPassword}
               type="text"
@@ -39,7 +64,7 @@ export const Login = () => {
             />
           </div>
           <div className="login__item login__btn">
-            <input disabled type="submit" />
+            <input type="submit" value="Продолжить" />
           </div>
           <div className="login__item login__register">
             <span>Еще нет аккаунта?</span>
